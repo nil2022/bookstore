48e4717eb6c4e99e634ca9e83e446cf615b266fb
Nilanjan Haldar <nil.haldar@gmail.com>
2024-03-02 01:50:50 +0530

    .env config fix

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-require('dotenv').config({
-  path: `.env.development`
-});  // import all environment variables
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 const { DB_URL } = require('./configs/db.config')
 const PORT = process.env.PORT || 8001
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json()) // parse JSON data & add it to the request.body object
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))
+app.use(express.json({ limit: '16kb' })) // parse JSON data & add it to the request.body object
 
 mongoose.connect(DB_URL)
 // FIRST CONNECT TO MONGODB THEN START LISTENING TO REQUESTS
@@ -23,8 +20,11 @@ mongoose.connect(DB_URL)
     console.log("Can't connect to DB:", error.message)
   })
 
-app.get('/', (req, res) => {
-    res.status(200).send('App running successfully')
+app.get('/health', (req, res) => {
+    res.status(200).send({
+      success: true,
+      message: 'Backend is up and running!'
+    })
 })
 
 const authRoutes = require('./routes/auth.routes')
